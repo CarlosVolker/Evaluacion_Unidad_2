cd3055907c831f9f72a50ced71f09ef56fd189c2
CarlosVolker <143527813+CarlosVolker@users.noreply.github.com>
2024-01-06 22:36:40 -0300

    Evaluacion

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ph2.u2.gonzalovillagra',
-  appName: 'ph2-u2-gonzalovillagra',
+  appId: 'ph2.u2.carlosvega',
+  appName: 'ph2-u2-carlosvega',
   webDir: 'www',
   server: {
     androidScheme: 'https'
